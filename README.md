07a8be32278f1e05523fd93cc31b1d9b472c2f5f
trapezechic <ingridhoffman@me.com>
2020-03-26 12:07:23 -0700

    prompts for user input

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ so that I have quick access to emails and GitHub profiles
 ```
 WHEN I open the Node terminal
 THEN I am provided prompts to build my engineering team which consists of a manager, and any number of engineers or interns
-WHEN I follow the prompts for information for my role as Manager
+WHEN I follow the prompts for my role as Manager
 THEN I am asked for my name, ID, email, and office number
 WHEN I complete my information
 THEN I am asked to select the role of my next team member
@@ -43,6 +43,7 @@ THEN the input is validated for proper expected format
 1. Run tests provided to understand required code functionality
 2. Create code for each class to pass all tests
 3. Create code for html creation using temp team profiles
+4. Create inquirer code for getting and saving user info
 
 ### Priorities
 
--- app.js
+++ app.js
@@ -1,31 +1,118 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+// Required external modules
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+
+// Required internal modules
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const render = require("./lib/htmlRenderer");
 
 // WHY?
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+// Variable to hold team members
+const team = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-// Temporary team
-const tempManager = new Manager("Iryna", 1, "iryna@email.com", 100);
-const tempEngineer = new Engineer("Tim", 2, "tim@email.com", "timUser");
-const tempIntern = new Intern("Forrest", 3, "forrest@email.com", "UofO");
-const team = [tempManager, tempEngineer, tempIntern];
+// USER PROMPTS
+// general Employee questions
+const employeeQuestions = [
+	{
+		type: "employeeInfo",
+		name: "name",
+		message: "Name:"
+	},
+	{
+		type: "employeeInfo",
+		name: "id",
+		message: "ID:"
+	},
+	{
+		type: "employeeInfo",
+		name: "email",
+		message: "Email:"
+	}
+];
+// Manager questions
+const managerQuestions = [
+	{
+		type: "employeeInfo",
+		name: "officeNumber",
+		message: "Office number:"
+	}
+];
+// Engineer questions
+const engineerQuestions = [
+	{
+		type: "employeeInfo",
+		name: "github",
+		message: "GitHub username:"
+	}
+];
+// Intern questions
+const internQuestions = [
+	{
+		type: "employeeInfo",
+		name: "school",
+		message: "School attending:"
+	}
+];
+// Next team member to be added
+const nextTeamMember = [
+	{
+		type: "list",
+		message: "What team member would you like to add next?",
+		name: "next",
+		choices: ["Engineer", "Intern", "No more team members"]
+	}
+];
 
-// Render HTML
-const html = render(team);
+// MAIN FUNCTION
+// Async wrapper function to await prompts
+async function createTeam() {
+	// Start with Manager
+	let role = "Manager";
+	// Until user indicates there are no more team members
+	while (role != "No more team members") {
+		// Prompt user for information about employee
+		const employeeInfo = await inquirer.prompt(employeeQuestions);
+		// Prompt for role specific information and create team member
+		let specialInfo;
+		let newTeamMember;
+		switch (role) {
+			case "Manager":
+				specialInfo = await inquirer.prompt(managerQuestions);
+				newTeamMember = new Manager(employeeInfo.name, employeeInfo.id, employeeInfo.email, specialInfo.officeNumber);
+				break;
+			case "Engineer":
+				specialInfo = await inquirer.prompt(engineerQuestions);
+				newTeamMember = new Engineer(employeeInfo.name, employeeInfo.id, employeeInfo.email, specialInfo.github);
+				break;
+			case "Intern":
+				specialInfo = await inquirer.prompt(internQuestions);
+				newTeamMember = new Intern(employeeInfo.name, employeeInfo.id, employeeInfo.email, specialInfo.school);
+				break;
+			default:
+				console.log("Oops! Something went wrong");
+		}
+		// Add new team member to team
+		team.push(newTeamMember);
+		// Get and set next team member role
+		const answer = await inquirer.prompt(nextTeamMember);
+		role = answer.next;
+	}
+	// Render HTML
+	const html = render(team);
 
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not. WHY? If we setup the folder system?
+	// Hint: you may need to check if the `output` folder exists and create it if it
+	// does not. WHY? If we setup the folder system?
 
-fs.writeFile(outputPath, html, err => {
-	if (err) throw err;
-	console.log(`File ${path.basename(outputPath)} has been saved!`);
-});
+	// Create HTML file
+	fs.writeFile(outputPath, html, err => {
+		if (err) throw err;
+		console.log(`File ${path.basename(outputPath)} has been saved!`);
+	});
+}
+createTeam();
